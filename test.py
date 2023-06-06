d412aca0f73912f75f56e43c7e867815cc0e787a
FURY <131582571+TEAM-CIPHER-404@users.noreply.github.com>
2023-06-06 13:23:50 +0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+try:
+    import subprocess
+except ImportError:
+    print("\n  installing subprocess!...\n")
+    os.system("pip install subprocess")
+
+
+test = subprocess.check_output('getprop ro.product.cpu.abilist',shell=True).decode('utf-8').replace('\n','')
